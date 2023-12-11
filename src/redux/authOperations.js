e278c9197976bf1f71ee0611da68be2e8872f81a
Soundlover1984 <denys.kovtun@gmail.com>
2023-12-11 22:42:39 +0200

    authentifiacation redux logic added

---
+++ src/redux/authOperations.js
@@ -0,0 +1,60 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile,
+  } from "firebase/auth";
+  import { createAsyncThunk } from "@reduxjs/toolkit";
+  import { auth } from "firebase/firebase";
+
+  export const registerUser = createAsyncThunk(
+    "auth/registerUser",
+    async (newUserData, { rejectWithValue }) => {
+      console.log(newUserData);
+      try {
+        await createUserWithEmailAndPassword(
+          auth,
+          newUserData.email,
+          newUserData.password
+        );
+
+        await updateProfile(auth.currentUser, {
+          displayName: newUserData.username,
+        });
+        const { uid, displayName, email } = auth.currentUser;
+        return { uid, displayName, email };
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
+
+  export const logInUser = createAsyncThunk(
+    "auth/logInUser",
+    async (userData, { rejectWithValue }) => {
+      try {
+        const { user } = await signInWithEmailAndPassword(
+          auth,
+          userData.email,
+          userData.password
+        );
+        const { uid, displayName, email } = user;
+        console.log("currentUser", uid, displayName, email);
+        return { uid, displayName, email };
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
+
+  export const logOut = createAsyncThunk(
+    "auth/logOut",
+    async (_, { rejectWithValue }) => {
+      try {
+        await signOut(auth);
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
+
