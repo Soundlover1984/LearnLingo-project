55c30c0091e4b967567f75e7625f014f92c1f8ed
Denys Kovtun <denys.kovtun@gmail.com>
2024-01-05 21:22:49 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,17 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  databaseURL:
-    'https://learnlingo-project-default-rtdb.europe-west1.firebasedatabase.app/',
-
-  apiKey: 'AIzaSyAw9-LDUKOzMFUHun3SsjB76kw0g2_ElA0',
-  authDomain: 'learnlingo-project.firebaseapp.com',
-  projectId: 'learnlingo-project',
-  storageBucket: 'learnlingo-project.appspot.com',
-  messagingSenderId: '727258100803',
-  appId: '1:727258100803:web:a7a67c908b0316157fd0f6',
-  measurementId: 'G-VFSQ9CYMSF',
+  apiKey: 'AIzaSyC3CYPUxu5kyHNi64Kden-mHOlVfrj84L0',
+  authDomain: 'learnlingo-e367c.firebaseapp.com',
+  databaseURL: 'https://learnlingo-e367c-default-rtdb.firebaseio.com',
+  projectId: 'learnlingo-e367c',
+  storageBucket: 'learnlingo-e367c.appspot.com',
+  messagingSenderId: '531778231414',
+  appId: '1:531778231414:web:b0a4dce271323f79149357',
+  measurementId: 'G-BNE4XJC73Q',
 };
+
 
 const app = initializeApp(firebaseConfig);
 
